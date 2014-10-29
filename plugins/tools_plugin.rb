8b624347272568a357142bfed798a1570ec9e898
Mark Glen Fletcher <markglenfletcher@googlemail.com>
2014-10-29 17:37:24 +0000

    The ToolsPlugin class is a test harness to aid the development of the project.
    This class will handle the basic functions of the bot, such as:
    
     - Connecting to and registering with the IRC network
     - Joining Channels
    
    Yet to determine whether this class should be responsible for intercepting commands for the bot whilst running.
    At the minute it is just driving the development of new message classes as and when they are needed.

--- plugins/tools_plugin.rb
+++ plugins/tools_plugin.rb
@@ -1,66 +1,34 @@
 require_relative '../lib/irc_plugin'
 
 class ToolsPlugin < IrcPlugin
-  attr_reader :options
-
-  USER_COMMAND = "USER %{user_name} %{host_name} %{server_name} :%{real_name}"
-  PASS_COMMAND = "PASS %{secret_pass}"
-  NICK_COMMAND = "NICK %{nick}"
-  QUIT_COMMAND = "QUIT"
-
   def initialize(options = {})
     @options = options
     @registered = false
+    @sent_registration = false
     @connected = false
   end
 
   def on_notice_messages(message)
-    respond_with_registration unless @registered
+    respond_with_registration unless @sent_registration
   end
 
-  def on_mode_messages(message)
-    respond_to_connected if is_connection_message && !@connected
-  end
-
-  def on_ping_messages(message)
-    "PONG"
+  def on_001_messages(message)
+    @registered = true
+    join_channel('#orc_tools')
   end
 
   private
 
-  def user_command(options = {})
-    USER_COMMAND % options
-  end
-
-  def pass_command(options = {})
-    PASS_COMMAND % options
-  end
-
-  def nick_command(options = {})
-    NICK_COMMAND % options
-  end
-
-  def quit_command
-    QUIT_COMMAND
-  end
-
   def respond_with_registration
-    @registered = true
+    @sent_registration = true
     [
-      pass_command(:secret_pass => options[:pass]),
-      nick_command(:nick => options[:nick_name]),
-      user_command(:user_name => options[:user_name],
-        :host_name => options[:host_name],
-        :server_name => options[:server_name],
-        :real_name => options[:real_name])
+      PassMessage.new(:password => @options[:pass]).to_s,
+      NickMessage.new(:nickname => @options[:nick_name]).to_s,
+      UserMessage.new(:nickname => @options[:nick_name], :mode => '8', :realname => @options[:real_name]).to_s
     ]
   end
 
-  def is_connection_message?(message)
-    (/:rubybottesting MODE rubybottesting :+i/ =~ message).nil?
-  end
-
-  def respond_to_connected
-    @connected = true
+  def join_channel(channel_name)
+    JoinMessage.new(:channel => channel_name).to_s
   end
 end
