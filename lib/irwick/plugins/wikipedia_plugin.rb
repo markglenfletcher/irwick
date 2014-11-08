8e33ea920af618fb009b9be4f21ecc052365aac5
Mark Glen Fletcher <markglenfletcher@googlemail.com>
2014-11-08 19:51:05 +0000

    Add wikipedia plugin for demonstrating how to build a simple plugin

--- lib/irwick/plugins/wikipedia_plugin.rb
+++ lib/irwick/plugins/wikipedia_plugin.rb
@@ -1,19 +1,50 @@
+require 'net/http'
+require 'json'
+
 class WikipediaPlugin < IrcPlugin
   def initialize(options = {})
+    @wikipedia_call = options[:wikipedia_call] || WikipediaCall.new
   end
 
   def on_privmsg_messages(message)
     if matches = /!(?<trigger>\S+) (?<query>.+)/.match(message.message)
       if matches[:trigger] == 'wiki'
-        respond_to_trigger(matches[:query])
+        wiki_response = respond_to_trigger(matches[:query])
+        IrcTools::PrivmsgMessage.new(:recipient => message.recipient, :message => wiki_response.slice(0,400))
       end
     end
   end
 
   def respond_to_trigger(query)
-    WikipediaCall.query(query)
+    @wikipedia_call.query(query)
   end
 end
 
 class WikipediaCall
+  WIKIPEDIA_EXTRACTS_QUERY_URL = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro='
+
+  def query(query)
+    begin
+      json_response = Net::HTTP.get build_uri_for(query)
+      dissect_json json_response
+    rescue KeyError
+      'Could not parse the JSON response from Wikipedia'
+    rescue Exception => e
+      "Can't do it!"
+    end
+  end
+
+  def build_uri_for(query)
+    URI(WIKIPEDIA_EXTRACTS_QUERY_URL + "&titles=#{query}")
+  end
+
+  def dissect_json(json_string)
+    # Strip HTML tags from the string
+    json_string.gsub!(/<\/?[^>]*>/, "")
+    json = JSON.parse(json_string)
+    page_key = json.fetch('query').fetch('pages').keys[0]
+    extract = json.fetch('query').fetch('pages').fetch(page_key).fetch('extract')
+    # Strip new lines
+    extract.gsub("\n",'')
+  end
 end
--- test/wikipedia_plugin_test.rb
+++ test/wikipedia_plugin_test.rb
@@ -1,21 +1,80 @@
 require 'test_helper'
+require 'net/http'
 
 class WikipediaPluginTest < Minitest::Test
+  def setup
+    @wikipedia_call = mock('wiki_call')
+  end
+
   def test_detect_the_correct_trigger
-    plugin = WikipediaPlugin.new
+    plugin = WikipediaPlugin.new(:wikipedia_call => @wikipedia_call)
+    plugin.expects(:respond_to_trigger).with('wikipedia').returns('wikipedia')
+
     trigger_message = mock('message')
     trigger_message.expects(:message).returns('!wiki wikipedia')
-    plugin.expects(:respond_to_trigger).with('wikipedia')
+    trigger_message.expects(:recipient).returns('#channel')
 
     plugin.on_privmsg_messages(trigger_message)
   end
 
   def test_respond_to_trigger_calls_wiki_api
-    plugin = WikipediaPlugin.new
     query = 'wikipedia'
     query_result = 'wikipedia response'
-    WikipediaCall.expects(:query).with('wikipedia').returns(query_result)
+    @wikipedia_call.expects(:query).with('wikipedia').returns(query_result)
+    plugin = WikipediaPlugin.new(:wikipedia_call => @wikipedia_call)
 
     assert_equal plugin.respond_to_trigger(query), query_result
+  end
+end
+
+class WikipediaCallTest < Minitest::Test
+  def setup
+    @wikipedia_call = WikipediaCall.new
+    @json_response = <<-EOF
+      {
+          "query": {
+              "pages": {
+                  "3850": {
+                      "pageid": 3850,
+                      "ns": 0,
+                      "title": "Baseball",
+                      "extract": "<p><b>Baseball</b>"
+                  }
+              }
+          }
+      }
+    EOF
+    @json_response.strip!
+    @expected_result = 'Baseball'
+    @uri = URI('http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Baseball')
+  end
+
+  def test_build_uri_is_as_expected
+    assert_equal @wikipedia_call.build_uri_for('Baseball'), @uri
+  end
+
+  def test_query_calls_api
+    Net::HTTP.expects(:get).with(@uri).returns(@json_response)
+    @wikipedia_call.query('Baseball')
+  end
+
+  def test_query_fails_is_handled
+    Net::HTTP.expects(:get).with(@uri).raises(Exception)
+    assert_equal "Can't do it!", @wikipedia_call.query('Baseball')
+  end
+
+  def test_query_returns_correct_string_upon_success
+    Net::HTTP.expects(:get).with(@uri).returns(@json_response)
+    assert_equal @expected_result, @wikipedia_call.query('Baseball')
+  end
+
+  def test_dissect_json_pulls_out_extracts
+    assert_equal @expected_result, @wikipedia_call.dissect_json(@json_response)
+  end
+
+  def test_query_handles_keyerror
+    Net::HTTP.expects(:get).with(@uri).returns(@json_response)
+    @wikipedia_call.expects(:dissect_json).with(@json_response).raises(KeyError)
+    assert_equal 'Could not parse the JSON response from Wikipedia', @wikipedia_call.query('Baseball')
   end
 end
